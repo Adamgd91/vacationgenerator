146df3878ea117f8f4b7523f47c8bc06cb448d38
Adam Davidson <agdavidson91@gmail.com>
2022-02-25 17:37:50 -0600

    added the const of the 4 Options

---
+++ app.js
@@ -0,0 +1,11 @@
+"use script";
+const destinationsOptions = ["Utah", "Texas", "Minnesota", "Georgia", "Kansas"];
+const restaurantsOptions = [
+  "Subway",
+  "Burger King",
+  "Chipotle",
+  "McDs",
+  "Zaxby's",
+];
+const transportationModesOptions = ["Car", "Bike", "Bus", "Plane", "Scooter"];
+const entertainmentOptions = ["Movie", "Beach", "Mall", "Golfing", "Concert"];
