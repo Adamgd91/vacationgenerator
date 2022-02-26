1e34d2d5eeb92d1d016604264d0af54aa2f37708
Adam Davidson <agdavidson91@gmail.com>
2022-02-25 19:31:40 -0600

    added other functions for option selections and DayTrips for those selections

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 "use script";
 const destinationsOptions = ["Utah", "Texas", "Minnesota", "Georgia", "Kansas"];
-const restaurantsOptions = [
-  "Subway",
-  "Burger King",
-  "Chipotle",
-  "McDs",
-  "Zaxby's",
-];
+const restaurantsOptions = ["Subway", "Burger King", "Chipotle", "McDs", "Zax"];
 const transportationOptions = ["Car", "Bike", "Bus", "Plane", "Scooter"];
 const entertainmentOptions = ["Movie", "Beach", "Mall", "Golfing", "Concert"];
 
@@ -34,11 +28,45 @@ function makeDestinationSelections() {
   }
 }
 
+function makeRestaurantSelections() {
+  const randomRestaurantOptions = getRandomValue(restaurantsOptions);
+  const userChoice = prompt(`Do you want to go to ${randomRestaurantOptions}?`);
+  if (userChoice.toLowerCase() === "yes") {
+    return randomRestaurantOptions;
+  } else {
+    return makeRestaurantSelections(restaurantsOptions);
+  }
+}
+
+function makeTransportationSelections() {
+  const randomTransportationOptions = getRandomValue(transportationOptions);
+  const userChoice = prompt(
+    `Do you want to go to ${randomTransportationOptions}?`
+  );
+  if (userChoice.toLowerCase() === "yes") {
+    return randomTransportationOptions;
+  } else {
+    return makeTransportationSelections(transportationOptions);
+  }
+}
+
+function makeEntertainmentSelections() {
+  const randomEntertainmentOptions = getRandomValue(entertainmentOptions);
+  const userChoice = prompt(
+    `Do you want to go to ${randomEntertainmentOptions}?`
+  );
+  if (userChoice.toLowerCase() === "yes") {
+    return randomEntertainmentOptions;
+  } else {
+    return makeEntertainmentSelections(entertainmentOptions);
+  }
+}
+
 function dayTrip() {
   trip.destination = makeDestinationSelections(destinationsOptions);
-  //   trip.restuarant = makeRestaurantSelections(restaurantsOptions);
-  //   trip.transportation = makeTransportationSelection(transportationOptions);
-  // trip.entertainment = makeEntertainmentSelections(entertainmentOptions);
+  trip.restuarant = makeRestaurantSelections(restaurantsOptions);
+  trip.transportation = makeTransportationSelections(transportationOptions);
+  trip.entertainment = makeEntertainmentSelections(entertainmentOptions);
   console.log(trip);
 }
 
